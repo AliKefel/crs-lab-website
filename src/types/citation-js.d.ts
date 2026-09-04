5be939fac2b2c60496092c15e1593c0cb045ec65
Ali Kefel <alikefel114@gmail.com>
2026-09-04 03:12:29 -0700

    added moduel

---
+++ src/types/citation-js.d.ts
@@ -0,0 +1,8 @@
+declare module '@citation-js/core' {
+  export class Cite {
+    constructor(data?: unknown, options?: unknown);
+    data: any[];
+  }
+}
+
+declare module '@citation-js/plugin-bibtex';
